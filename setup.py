bb9a25201ca4c1025c03a339c5e4718390a7b240
Abhinav Srivastava <atg271@gmail.com>
2023-07-15 05:41:31 +0530

    wip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='abi_parser',
+    version='1.0.0',
+    description='A Python package to parse Ethereum contract ABI files',
+    author='August Radjoe',
+    author_email='atg271@gmail.com',
+    packages=['abi_parser'],
+    install_requires=[],  # Add any dependencies here
+)
